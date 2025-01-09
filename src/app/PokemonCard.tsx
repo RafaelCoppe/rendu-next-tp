6566060e8487dde699f199191d080dc0a25c5e07
Rafael COPPE <rafael.coppe.jr@gmail.com>
2025-01-09 13:02:18 +0100

    commit 1 - main page

---
+++ src/app/PokemonCard.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+interface PokemonType {
+    name: string;
+    image: string;
+}
+
+interface PokemonCardProps {
+  pokedexId: number;
+  name: string;
+  image: string;
+  types: PokemonType[];
+}
+
+const PokemonCard: React.FC<PokemonCardProps> = ({ pokedexId, name, image, types }) => {
+    return (
+      <div className="pokemon-card" key={pokedexId}>
+        <div className="pokemon-image">
+          <img src={image} alt={name} />
+        </div>
+        <div className="pokemon-info">
+          <span className="pokemon-number">#{pokedexId}</span>
+          <span className="pokemon-name">{name}</span>
+        </div>
+        <div className="pokemon-types">
+            {types.map((type, typeId) => (
+                <img key={`${pokedexId}_${typeId}`} src={type.image} alt={type.name} className='pokemon-types-image' />
+            ))}
+        </div>
+      </div>
+    );
+  };
+
+export default PokemonCard;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,110 @@
+"use client"
+import React, { useEffect, useState, useRef } from 'react';
 import Image from "next/image";
+import './app.css';
+import PokemonCard from './PokemonCard';
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [pokemons, setPokemons] = useState([]); // Liste des Pokémon
+  const [isLoading, setIsLoading] = useState(false); // Indicateur de chargement
+  const [page, setPage] = useState(1); // Page courante
+  const [limit, setLimit] = useState("50"); // Limite des résultats par page
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const sentinelRef = useRef(null); // Référence pour l'observateur
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  // Charger les Pokémon
+  const fetchPokemons = async (currentPage) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(`https://nestjs-pokedex-api.vercel.app/pokemons?limit=${limit}&page=${currentPage}`);
+      const data = await response.json();
+      if (currentPage > 1) {
+        await delay(1000); // Attente pour un effet visuel
+      }
+      setPokemons((prevPokemons) => [...prevPokemons, ...data]); // Ajouter les nouveaux Pokémon
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  // Charger la première page au montage
+  useEffect(() => {
+    fetchPokemons(page);
+  }, [page]);
+
+  // Observateur pour détecter la visibilité du sentinel
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        const entry = entries[0];
+        if (entry.isIntersecting && !isLoading) {
+          setPage((prevPage) => prevPage + 1); // Charger la page suivante
+        }
+      },
+      { threshold: 1.0 } // Observer lorsque l'élément est totalement visible
+    );
+
+    const sentinel = sentinelRef.current;
+    if (sentinel) {
+      observer.observe(sentinel);
+    }
+
+    return () => {
+      if (sentinel) {
+        observer.unobserve(sentinel);
+      }
+    };
+  }, [isLoading]);
+
+  return (
+    <div className="app">
+      <div className="topbar">
+        <div className="logo">
+          <Image src="/pokeball.png" alt="Pokeball" width={16} height={16} />
+          <span>Pokedex</span>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="filters">
+          <input type="text" placeholder="Rechercher un Pokémon" className="search-input" />
+          <select multiple className="multi-select">
+            <option value="fire">Feu</option>
+            <option value="water">Eau</option>
+            <option value="grass">Plante</option>
+          </select>
+          <select
+            className="limit_select"
+            onChange={(e) => {
+              setLimit(e.target.value);
+              setPokemons([]);
+              setPage(1);
+            }}
+          >
+            <option value="50">Limit</option>
+            <option value="10">10</option>
+            <option value="20">20</option>
+            <option value="50">50</option>
+            <option value="100">100</option>
+          </select>
+        </div>
+      </div>
+      <div className="main">
+        <div className="pokemons">
+          {pokemons.map((pokemon) => (
+            <PokemonCard
+              key={pokemon.pokedexId}
+              pokedexId={pokemon.pokedexId}
+              name={pokemon.name}
+              image={pokemon.image}
+              types={pokemon.types}
+            />
+          ))}
+        </div>
+        {isLoading && <div className="loading">Chargement...</div>}
+        <div ref={sentinelRef} className="sentinel"></div> {/* Sentinel */}
+      </div>
     </div>
   );
 }
